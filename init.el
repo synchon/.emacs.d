3553915193cf91aff500655f1590d3f89f07b21c
Synchon Mandal <synchon@protonmail.com>
2022-12-14 21:18:47 +0100

    update: enable DAP via `dap-mode`

--- init.el
+++ init.el
@@ -622,6 +622,23 @@
 ;;   :config
 ;;   (add-to-list 'auto-mode-alist '("\\.yml\\'". yaml-mode)))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; DAP (Debugging Adapter Protocol) support
+(use-package dap-mode
+  :ensure t
+  :config
+  (dap-mode 1)
+  ;; The modes below are optional
+  (dap-ui-mode 1)
+  ;; enables mouse hover support
+  (dap-tooltip-mode 1)
+  ;; use tooltips for mouse hover
+  ;; if it is not enabled `dap-mode' will use the minibuffer.
+  (tooltip-mode 1)
+  ;; displays floating panel with debug buttons
+  ;; requies emacs 26+
+  (dap-ui-controls-mode 1)
+  (require 'dap-python))
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; UI
 ;; Theme
 (use-package color-theme-sanityinc-tomorrow
