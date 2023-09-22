7163dc0b6576929bf1ca181ef0f88af8f7266158
Synchon Mandal <synchonmandal@gmail.com>
2023-09-22 22:24:46 +0530

    update: add marginalia

--- init.el
+++ init.el
@@ -282,7 +282,16 @@
   :custom
   (corfu-cycle t))       ;; Enable cycling for `corfu-next/previous'
 
+;; Enable rich annotations in the minibuffer
+(use-package marginalia
   :ensure t
+  ;; Bind `marginalia-cycle' locally in the minibuffer. To make the binding
+  ;; available in the *Completions* buffer, add it to the
+  ;; `completion-list-mode-map'.
+  :bind (:map minibuffer-local-map
+         ("M-A" . marginalia-cycle))
+  :init
+  (marginalia-mode))
 
   :ensure t
 
